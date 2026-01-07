38ddccf79e27d2d6bca456a437f4fe264472e420
riricat-sketch <ri.audoire@octopusev.com>
2026-01-07 14:11:32 +0000

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>My Dictionary App</h1>
+      <p>Search for word definitions</p>
+    </header>
+  );
+}
+
+export default Header;
